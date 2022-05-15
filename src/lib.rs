90240e9bc6bfff9b39e9ae10f30a7ad6cbac8039
Andrew Rondeau <git@andrewrondeau.com>
2022-05-15 16:39:08 -0400

    Implemented a streaming reader

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,8 @@ pub mod wave_header;
 pub mod wave_reader;
 
 use reader::ReadEx;
-use wave_header::{SampleFormat, WavHeader};
-use wave_reader::{OpenWav, RandomAccessWavReader};
+use wave_header::*;
+use wave_reader::*;
 
 pub fn from_file(file_path: &str) -> Result<OpenWav<BufReader<File>>> {
     let file = File::open(file_path)?;
@@ -59,7 +59,7 @@ mod tests {
     #[test]
     fn read_float_sanity() {
         let open_wav = from_file("test_data/short_float.wav").unwrap();
-        let wave_reader_float = open_wav.as_random_access_float().unwrap();
+        let wave_reader_float = open_wav.get_random_access_float_reader().unwrap();
         assert_eq!(SampleFormat::Float, wave_reader_float.info().sample_format());
         assert_eq!(1, wave_reader_float.info().channels());
         assert_eq!(32, wave_reader_float.info().bits_per_sample());
@@ -67,14 +67,14 @@ mod tests {
         assert_eq!(1267, wave_reader_float.info().len_samples());
 
         let open_wav = from_file("test_data/short_24.wav").unwrap();
-        let read_float_result = open_wav.as_random_access_float();
+        let read_float_result = open_wav.get_random_access_float_reader();
         match read_float_result {
             Result::Err(_) => {},
             _ => panic!("Should not be able to read file")
         }
 
         let open_wav = from_file("test_data/short_16.wav").unwrap();
-        let read_float_result = open_wav.as_random_access_float();
+        let read_float_result = open_wav.get_random_access_float_reader();
         match read_float_result {
             Result::Err(_) => {},
             _ => panic!("Should not be able to read file")
@@ -84,7 +84,7 @@ mod tests {
     #[test]
     fn read_float() {
         let open_wav = from_file("test_data/short_float.wav").unwrap();
-        let mut wave_reader_float = open_wav.as_random_access_float().unwrap();
+        let mut wave_reader_float = open_wav.get_random_access_float_reader().unwrap();
 
         let expected_sample = f32::from_le_bytes([0x6D, 0xB4, 0xA7, 0xBC]);
         let actual_sample = wave_reader_float.read_sample(0, 0).unwrap();
@@ -92,10 +92,62 @@ mod tests {
 
         let expected_sample = f32::from_le_bytes([0x02, 0xC6, 0x81, 0xBC]);
         let actual_sample = wave_reader_float.read_sample(1, 0).unwrap();
-        assert_eq!(expected_sample, actual_sample, "Wrong sample read at sample 0, channel 0");
+        assert_eq!(expected_sample, actual_sample, "Wrong sample read at sample 1, channel 0");
 
         let expected_sample = f32::from_le_bytes([0xA0, 0xB5, 0x31, 0xBC]);
         let actual_sample = wave_reader_float.read_sample(wave_reader_float.info().len_samples() - 1, 0).unwrap();
-        assert_eq!(expected_sample, actual_sample, "Wrong sample read at sample 0, channel 0");
+        assert_eq!(expected_sample, actual_sample, "Wrong sample read at sample 1266, channel 0");
+    }
+
+    #[test]
+    fn stream_float_sanity() {
+        let open_wav = from_file("test_data/short_float.wav").unwrap();
+        let wave_reader_float = open_wav.get_stream_float_reader().unwrap();
+        assert_eq!(SampleFormat::Float, wave_reader_float.info().sample_format());
+        assert_eq!(1, wave_reader_float.info().channels());
+        assert_eq!(32, wave_reader_float.info().bits_per_sample());
+        assert_eq!(48000, wave_reader_float.info().sample_rate());
+        assert_eq!(1267, wave_reader_float.info().len_samples());
+
+        let open_wav = from_file("test_data/short_24.wav").unwrap();
+        let stream_float_result = open_wav.get_stream_float_reader();
+        match stream_float_result {
+            Result::Err(_) => {},
+            _ => panic!("Should not be able to read file")
+        }
+
+        let open_wav = from_file("test_data/short_16.wav").unwrap();
+        let stream_float_result = open_wav.get_stream_float_reader();
+        match stream_float_result {
+            Result::Err(_) => {},
+            _ => panic!("Should not be able to read file")
+        }
+    }
+
+    #[test]
+    fn stream_float() {
+        let mut current_sample: usize = 0;
+        let open_wav = from_file("test_data/short_float.wav").unwrap();
+        for samples_result in open_wav.get_stream_float_reader().unwrap() {
+            let samples = samples_result.unwrap();
+
+            assert_eq!(1, samples.len(), "Wrong number of samples");
+
+            if current_sample == 0 {
+                let expected_sample = f32::from_le_bytes([0x6D, 0xB4, 0xA7, 0xBC]);
+                assert_eq!(expected_sample, samples[0], "Wrong sample read at sample 0, channel 0");
+        
+            } else if current_sample == 1 {
+                let expected_sample = f32::from_le_bytes([0x02, 0xC6, 0x81, 0xBC]);
+                assert_eq!(expected_sample, samples[0], "Wrong sample read at sample 1, channel 0");
+            } else if current_sample == 1266 {
+                let expected_sample = f32::from_le_bytes([0xA0, 0xB5, 0x31, 0xBC]);
+                assert_eq!(expected_sample, samples[0], "Wrong sample read at sample 1266, channel 0");
+            }
+
+            current_sample += 1;
+        }
+
+        assert_eq!(1267, current_sample, "Wrong number of samples read");
     }
 }
--- src/wave_reader.rs
+++ src/wave_reader.rs
@@ -4,20 +4,11 @@ use crate::ReadEx;
 use crate::SampleFormat;
 use crate::WavHeader;
 
-pub struct OpenWav<TReader: Read + Seek> {
+pub struct OpenWav<TReader: Read> {
     reader: TReader,
     header: WavHeader,
     data_start: u32,
     data_length: u32,
-}
-
-pub struct RandomAccessWavReaderFloat<TReader: Read + Seek> {
-    open_wav: OpenWav<TReader>
-}
-
-pub trait RandomAccessWavReader<T, TReader: Read + Seek> {
-    fn info(&self) -> &OpenWav<TReader>;
-    fn read_sample(&mut self, sample: u32, channel: u16) -> Result<T>;
 }
 
 impl<TReader: Read + Seek> OpenWav<TReader> {
@@ -44,14 +35,29 @@ impl<TReader: Read + Seek> OpenWav<TReader> {
         })
     }
 
-    pub fn as_random_access_float(self) -> Result<RandomAccessWavReaderFloat<TReader>> {
+    fn assert_float(&self) -> Result<()> {
         if self.header.sample_format == SampleFormat::Float {
-            Ok(RandomAccessWavReaderFloat {
-                open_wav: self
-            })
+            Ok(())
         } else {
             Err(Error::new(ErrorKind::InvalidData, "Converting to float unsupported"))
         }
+    }
+
+    pub fn get_random_access_float_reader(self) -> Result<RandomAccessWavReaderFloat<TReader>> {
+        self.assert_float()?;
+
+        Ok(RandomAccessWavReaderFloat {
+            open_wav: self
+        })
+    }
+
+    pub fn get_stream_float_reader(self) -> Result<StreamWavReaderFloat<TReader>> {
+        self.assert_float()?;
+
+        Ok(StreamWavReaderFloat {
+            open_wav: self,
+            current_sample: 0
+        })
     }
 
     pub fn sample_format(&self) -> SampleFormat {
@@ -79,6 +85,15 @@ impl<TReader: Read + Seek> OpenWav<TReader> {
     }
 }
 
+pub trait RandomAccessWavReader<T, TReader: Read + Seek> {
+    fn info(&self) -> &OpenWav<TReader>;
+    fn read_sample(&mut self, sample: u32, channel: u16) -> Result<T>;
+}
+
+pub struct RandomAccessWavReaderFloat<TReader: Read + Seek> {
+    open_wav: OpenWav<TReader>
+}
+
 impl<TReader: Read + Seek> RandomAccessWavReader<f32, TReader> for RandomAccessWavReaderFloat<TReader> {
     fn info(&self) -> &OpenWav<TReader> {
         &(self.open_wav)
@@ -99,5 +114,47 @@ impl<TReader: Read + Seek> RandomAccessWavReader<f32, TReader> for RandomAccessW
         self.open_wav.reader.seek(SeekFrom::Start(position as u64))?;
         
         self.open_wav.reader.read_f32()
+    }
+}
+
+pub struct StreamWavReaderFloat<TReader: Read + Seek> {
+    open_wav: OpenWav<TReader>,
+    current_sample: u32,
+}
+
+pub trait StreamWavReader<T, TReader: Read + Seek> {
+    fn info(&self) -> &OpenWav<TReader>;
+}
+
+impl<TReader: Read + Seek> StreamWavReader<f32, TReader> for StreamWavReaderFloat<TReader> {
+    fn info(&self) -> &OpenWav<TReader> {
+        &(self.open_wav)
+    }
+}
+
+impl<TReader: Read + Seek> Iterator for StreamWavReaderFloat<TReader> {
+    type Item = Result<Vec<f32>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_sample >= self.open_wav.len_samples() {
+            None
+        } else {
+            let num_channels: usize = self.open_wav.channels().into();
+            let mut samples = vec![0f32; num_channels];
+
+            for channel in 0..num_channels {
+                samples[channel] = match self.open_wav.reader.read_f32() {
+                    Ok(sample) => sample,
+                    Err(err) => {
+                        self.current_sample = u32::MAX;
+                        return Some(Err(err))
+                    }
+                }
+            }
+
+            self.current_sample += 1;
+
+            Some(Ok(samples))
+        }
     }
 }
